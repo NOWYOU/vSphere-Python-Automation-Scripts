0daf6c37c7187ab24b63ccb532b12a7123b6a0f4
NOWYOU <43359644+NOWYOU@users.noreply.github.com>
2023-04-02 21:32:23 +0800

    Delete Datacenter
    Delete Datacenter

---
+++ v1/vCenter-Datacenter/DeleteDatacenter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : DeleteDatacenter.py
+# @Author: NOWSHUT
+# @Date  : 2023/4/2 18:35
+# @Desc  :
+# @Contact : nowshut@qq.com
+
+import time
+from vSphere_Automation_SDK.Connect_to_vCenter_Server import vsphere_client
+
+start_time = time.time()
+try:
+    delete_dc = vsphere_client.vcenter.Datacenter.delete("datacenter-1001")
+    print("Deleted successfully！")
+
+except Exception as err:
+    for i in err.messages:
+        id = i.id,
+        default_message = i.default_message
+        args = i.args
+        params = i.params
+        localized = i.localized
+    print("\033[1;31m Encountered an error, Please see the following information \033[0m",
+          "\n\tError Class:", id,
+          "\n\tMessage:", default_message,
+          "\n\tArgs:", args,
+          "\n\tParams:", params,
+          "\n\tLocalized:", localized,
+          "\nError Data:", err.data,
+          "\nError Type:", err.error_type
+          )
+end_time = time.time()
+run_time = end_time - start_time
+print("Used Time:".ljust(43), run_time)
